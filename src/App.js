4a74f9fe26321176f7e0b1853befceff6db7df7a
et41 <erenturan41@gmail.com>
2022-06-19 13:06:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <div className="hero">
+      <div className="navbar">
+        <div className="navbar--left">
+          <ul>
+            <img
+              className="logo"
+              src="https://www.etstur.com/resources_t/img/webpush-logo.png"
+            />
+            <li>Konser</li>
+            <li>Tiyatro</li>
+            <li>Fazlası...</li>
+          </ul>
+        </div>
+        <div className="navbar--right">
+          <ul>
+            <li>Kayıt Ol</li>
+          </ul>
+        </div>
+      </div>
+      <div className="header">
+        <h1>Her Etkinlik İçin</h1>
+        <h3>İstediğiniz bilete anında ulaşın!!</h3>
+      </div>
+     <div className="searchbar--main">
+       <span class="search--icon">🔎︎</span>
+       <input class="searchbar" type="search" name="Search" placeholder="İstediğiniz etkinliği arayın..." />
+
+      </div>
+      
+      </div>
+      <div className="browse">
+        <p>Etkinlikler </p>
+        <p>Şehir</p>
+        <div className="browse--buttons">
+          <button >Şehir Değiştir</button>
+          <button>Tarihe Göre Ara</button>
+        </div>
+      </div>
+      </div>
   );
 }
 
-export default App;
